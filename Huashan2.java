382b99f2f12c6db956e6f93734621a51ed16eee4
gaosen <991434187@qq.com>
2020-12-14 19:49:00 +0800

    I love you

---
+++ Huashan2.java
@@ -0,0 +1,8 @@
+public class Huashan2 {
+    public static void main(String[] args) {
+        System.out.println("华山2强");
+        System.out.println("华山2强");
+        System.out.println("华山2强");
+
+    }
+}
